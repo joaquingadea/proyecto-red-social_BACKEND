96a48caf579f4ccc8187d54fc199ecab4e873d2a
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 15:12:18 -0300

    Se realizaron las configuraciones basicas, declaracion de variables de entorno, conexion con la base de datos local, cambio de  dependencias, etc. La aplicación ya está lista para el comienzo del desarrollo.

---
+++ demo/src/test/java/com/red_social/demo/RedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red_social.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

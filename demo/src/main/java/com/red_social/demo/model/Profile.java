a9b5eb58f7873e1b526c498396880ae72795c065
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 21:38:53 -0300

    Se definió como se va a comportar Profile en la base de datos, junto con sus atributos, constructores y la relación 1 a 1 unidireccional profile -> user (el perfil conoce al usuario, pero el usuario no al perfil)

--- demo/src/main/java/com/red_social/demo/model/Profile.java
+++ demo/src/main/java/com/red_social/demo/model/Profile.java
@@ -1,4 +1,30 @@
 package com.red_social.demo.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "profiles")
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private LocalDate creationDate;
+
+
+    // FK user_id
+    // 1 a 1 -> profile -> user
+    @OneToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private UserSec user;
+
+    public Profile(){
+    }
+
+    public Profile(LocalDate creationDate, UserSec user) {
+        this.creationDate = creationDate;
+        this.user = user;
+    }
 }

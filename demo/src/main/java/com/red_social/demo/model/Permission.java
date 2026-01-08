16b5b5540edd7f5c3b560eb8efc68f02ec8d0fb4
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 22:08:57 -0300

    se definio la clase con sus atributos, constructoresy que se va a comportar como una entidad en la base de datos

--- demo/src/main/java/com/red_social/demo/model/Permission.java
+++ demo/src/main/java/com/red_social/demo/model/Permission.java
@@ -1,4 +1,19 @@
 package com.red_social.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "permissions")
 public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Permission() {
+    }
+
+    public Permission(String name) {
+        this.name = name;
+    }
 }

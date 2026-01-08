0e009065822d4901026187b01e8ab0542295be24
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 21:26:19 -0300

    Se definió la clase, como se va a comportar como una entidad en la base de datos, su constructor, sus atributos y la relacion N a 1 unidireccional messages -> profile

--- demo/src/main/java/com/red_social/demo/model/Message.java
+++ demo/src/main/java/com/red_social/demo/model/Message.java
@@ -1,4 +1,31 @@
 package com.red_social.demo.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String text;
+    private LocalDateTime creationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
+
+    public Message() {
+    }
+
+    public Message(String title, String text, LocalDateTime creationDate, Profile profile) {
+        this.title = title;
+        this.text = text;
+        this.creationDate = creationDate;
+        this.profile = profile;
+    }
+
 }

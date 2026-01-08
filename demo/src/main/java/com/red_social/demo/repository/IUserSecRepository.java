3c795371bfdd812fd351f973148a339b188157ba
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-08 16:03:19 -0300

    Se creo el metodo findUserSecEntityByUsername

--- demo/src/main/java/com/red_social/demo/repository/IUserSecRepository.java
+++ demo/src/main/java/com/red_social/demo/repository/IUserSecRepository.java
@@ -4,6 +4,9 @@ import com.red_social.demo.model.UserSec;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserSecRepository extends JpaRepository<UserSec,Long> {
+    Optional<UserSec> findUserSecEntityByUsername(String username);
 }

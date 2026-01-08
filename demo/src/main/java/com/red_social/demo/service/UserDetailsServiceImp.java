cf35c85e56f67d23c4c45e3f50e0333571e539c6
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-08 18:00:38 -0300

    se implemento correctamente el metodo loadUserByUsername

--- demo/src/main/java/com/red_social/demo/service/UserDetailsServiceImp.java
+++ demo/src/main/java/com/red_social/demo/service/UserDetailsServiceImp.java
@@ -3,10 +3,15 @@ package com.red_social.demo.service;
 import com.red_social.demo.model.UserSec;
 import com.red_social.demo.repository.IUserSecRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
@@ -19,12 +24,31 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
         UserSec userSec = userRepo.findUserSecEntityByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("El usuario " + username + " no existe"));
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
         // Conseguir los permisos -> almacenarlos en authorities (SimpleGrantedAuthority)
+
+        userSec.getRoleList().stream()
+                .flatMap(role -> role.getPermissionList().stream())
+                .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.getName())));
+
         // Conseguir los roles -> almacenarlos en authorities (SimpleGrantedAuthority)
+
+        userSec.getRoleList().stream()
+                .forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_".concat(role.getName()))));
 
         // Devolver el usuario como un UserDetails
 
-        return null;
+        return new User(
+                userSec.getUsername(),
+                userSec.getPassword(),
+                userSec.isEnabled(),
+                userSec.isAccountNotExpired(),
+                userSec.isCredentialNotExpired(),
+                userSec.isAccountNotLocked(),
+                authorities
+        );
 
     }
 }

8ff3d0ba778e90e4262390c36ed0e6feb638ebfb
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 21:55:23 -0300

    se declararon los constructores, sus atributos, como se va a comportar como entidad en la base de datos, se definio la tabla intermedia entre users y roles de la base de datos para la relacion N a N unidireccional users -> roles

--- demo/src/main/java/com/red_social/demo/model/UserSec.java
+++ demo/src/main/java/com/red_social/demo/model/UserSec.java
@@ -1,4 +1,41 @@
 package com.red_social.demo.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class UserSec {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JoinColumn(unique = true)
+    private String username;
+    private String password;
+    private boolean enabled;
+    private boolean accountNotExpired;
+    private boolean accountNotLocked;
+    private boolean credentialNotExpired;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role"
+            ,joinColumns = @JoinColumn(name = "user_id")
+            ,inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roleList = new HashSet<>();
+
+    public UserSec() {
+    }
+
+    public UserSec(String username, String password, boolean enabled, boolean accountNotExpired, boolean accountNotLocked, boolean credentialNotExpired, Set<Role> roleList) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.accountNotExpired = accountNotExpired;
+        this.accountNotLocked = accountNotLocked;
+        this.credentialNotExpired = credentialNotExpired;
+        this.roleList = roleList;
+    }
 }

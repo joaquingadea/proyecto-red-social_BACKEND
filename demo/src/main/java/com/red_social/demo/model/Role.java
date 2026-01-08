82ba7334547f8674a3ae2f2a19c4615d4a2dfa88
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-07 22:04:31 -0300

    Se definio la clase, sus constructores, sus atributos, como se va a manejar como una entidad en la base de datos, la relacion N a N unidireccional de roles a permisos y como va a ser la tabla intermedia 'role_permission'

--- demo/src/main/java/com/red_social/demo/model/Role.java
+++ demo/src/main/java/com/red_social/demo/model/Role.java
@@ -1,4 +1,27 @@
 package com.red_social.demo.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    
+    // Relacion N a N / roles -> permisos unidireccional
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "role_permission"
+    ,joinColumns = @JoinColumn(name = "role_id")
+    ,inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permission> permissionList= new HashSet<>();
+
+    public Role(String name, Set<Permission> permissionList) {
+        this.name = name;
+        this.permissionList = permissionList;
+    }
 }

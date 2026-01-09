50cf509bb376f7c278807ee5a299f3a496a98a00
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-09 16:50:14 -0300

    se implementó el metodo findPermissionByName

--- demo/src/main/java/com/red_social/demo/service/PermissionService.java
+++ demo/src/main/java/com/red_social/demo/service/PermissionService.java
@@ -1,7 +1,25 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.Permission;
+import com.red_social.demo.repository.IPermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PermissionService implements IPermissionService {
+
+    @Autowired
+    private IPermissionRepository permissionRepository;
+
+    @Override
+    public Optional<Permission> findPermissionByName(String name) {
+        return permissionRepository.findPermissionByName(name);
+    }
+
+    @Override
+    public Permission create(Permission permission) {
+        return permissionRepository.save(permission);
+    }
 }

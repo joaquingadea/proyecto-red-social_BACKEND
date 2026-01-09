973ec04105a9cc52157fd1ed1c27ac90b9fc57f4
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-09 16:50:48 -0300

    se implemento correctamente el endpoint createPermission

--- demo/src/main/java/com/red_social/demo/controller/PermissionController.java
+++ demo/src/main/java/com/red_social/demo/controller/PermissionController.java
@@ -1,7 +1,36 @@
 package com.red_social.demo.controller;
 
+import com.red_social.demo.model.Permission;
+import com.red_social.demo.service.IPermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/permission")
 public class PermissionController {
+
+    @Autowired
+    private IPermissionService permissionService;
+
+    @PostMapping //("/create")
+    public ResponseEntity createPermission(@RequestBody Permission permission){
+        Optional<Permission> permissionExist = permissionService.findPermissionByName(permission.getName());
+
+        if (permissionExist.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Ya existe el permiso "+ permission.getName());
+        }
+
+         // Si el permiso no existe lo crea
+        return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(permissionService.create(permission));
+
+    }
 }

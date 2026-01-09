eed1dc532bf7b9c73b8de23d792868256ce899f8
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-09 16:49:45 -0300

    se añadio el metodo findPermissionByName

--- demo/src/main/java/com/red_social/demo/service/IPermissionService.java
+++ demo/src/main/java/com/red_social/demo/service/IPermissionService.java
@@ -1,4 +1,11 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.Permission;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Optional;
+
 public interface IPermissionService {
+    Optional<Permission> findPermissionByName(String name);
+    Permission create(Permission permission);
 }

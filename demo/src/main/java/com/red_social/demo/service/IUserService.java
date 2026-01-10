5a87fd469eae3a562829814f6145d4a2848991d8
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-10 20:28:14 -0300

    creacion e implementacion de los metodos encriptPassword, findAll y create en las clases e interfaces UserService.java y IUserService.java

--- demo/src/main/java/com/red_social/demo/service/IUserService.java
+++ demo/src/main/java/com/red_social/demo/service/IUserService.java
@@ -1,4 +1,14 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.UserSec;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
 public interface IUserService {
+    String encriptPassword(String password);
+
+    List<UserSec> findAll();
+
+    UserSec create(UserSec userSec);
 }
--- demo/src/main/java/com/red_social/demo/service/UserService.java
+++ demo/src/main/java/com/red_social/demo/service/UserService.java
@@ -1,7 +1,32 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.UserSec;
+import com.red_social.demo.repository.IUserSecRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService implements IUserService{
+
+    @Autowired
+    private IUserSecRepository userRepo;
+
+    @Override
+    public String encriptPassword(String password) {
+        return new BCryptPasswordEncoder().encode (password);
+    }
+
+    @Override
+    public List<UserSec> findAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public UserSec create(UserSec userSec) {
+        return userRepo.save(userSec);
+    }
 }

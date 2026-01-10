9532f879216271117b58159eb13d0f478367bd28
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-09 21:47:30 -0300

    se crearon e implementaron los metodos existsByName y create

--- demo/src/main/java/com/red_social/demo/service/IRoleService.java
+++ demo/src/main/java/com/red_social/demo/service/IRoleService.java
@@ -1,4 +1,9 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.Role;
+import org.jspecify.annotations.Nullable;
+
 public interface IRoleService {
+    boolean existsByName(String name);
+    Role create(Role role);
 }
--- demo/src/main/java/com/red_social/demo/service/RoleService.java
+++ demo/src/main/java/com/red_social/demo/service/RoleService.java
@@ -1,7 +1,23 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.model.Role;
+import com.red_social.demo.repository.IRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService implements IRoleService{
+
+    @Autowired
+    private IRoleRepository roleRepository;
+
+    @Override
+    public boolean existsByName(String name) {
+        return roleRepository.existsByName(name);
+    }
+
+    @Override
+    public Role create(Role role) {
+        return roleRepository.save(role);
+    }
 }

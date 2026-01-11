de5f52c38ea16488fbeb3dd8eebd62b6b046de87
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-11 17:39:59 -0300

    se implementaron los dos endpoints correspondientes de UserController.java para dar de alta usuarios y obtener la lista de los creados

--- demo/src/main/java/com/red_social/demo/controller/UserController.java
+++ demo/src/main/java/com/red_social/demo/controller/UserController.java
@@ -1,7 +1,72 @@
 package com.red_social.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.red_social.demo.model.Role;
+import com.red_social.demo.model.UserSec;
+import com.red_social.demo.service.IRoleService;
+import com.red_social.demo.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RestController
+@PreAuthorize("permitAll()")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IRoleService roleService;
+
+    @GetMapping
+    public ResponseEntity getUsers(){
+        List<UserSec> users = userService.findAll();
+        if (users.isEmpty()){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("No existen usuarios todavia");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    //Creacion de user PRUEBA -> luego hay que transformarlo en register
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody UserSec userSec){
+
+        if (userSec.getPassword().length() < 8){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("La contraseña tiene que tener minimo 8 caracteres");
+        }
+
+        if (userSec.getRoleList().isEmpty()){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("La lista de roles esta vacia");
+        }
+        userSec.setPassword(userService.encriptPassword(userSec.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+
+        //Se guardan los roles que existen nada mas
+        for (Role role : userSec.getRoleList()){
+            if(roleService.existsById(role.getId())){
+                roles.add(role);
+            }
+        }
+        if (roles.isEmpty()){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Los roles pasados no existen");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.create(userSec));
+    }
 }

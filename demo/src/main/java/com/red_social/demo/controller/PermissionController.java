366f9177d52c4bcdd0f4d8afcc4063629aaa43fb
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-11 15:42:05 -0300

    se implementaron los endpoints getPermissions y getById en el PermissionController.java

--- demo/src/main/java/com/red_social/demo/controller/PermissionController.java
+++ demo/src/main/java/com/red_social/demo/controller/PermissionController.java
@@ -1,23 +1,46 @@
 package com.red_social.demo.controller;
 
 import com.red_social.demo.model.Permission;
+import com.red_social.demo.model.UserSec;
 import com.red_social.demo.service.IPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@PreAuthorize("permitAll()")
 @RequestMapping("/permission")
 public class PermissionController {
 
     @Autowired
     private IPermissionService permissionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Long id){
+
+        Optional<Permission> permission = permissionService.findById(id);
+
+        return permission.map(p -> ResponseEntity.status(HttpStatus.OK).body(p))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.CONFLICT).build());
+    }
+
+    @GetMapping
+    public ResponseEntity getPermissions(){
+        List<Permission> permissionsList = permissionService.getPermissions();
+
+        if(permissionsList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("No existen permisos todavia");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(permissionsList);
+    }
 
     @PostMapping //("/create")
     public ResponseEntity createPermission(@RequestBody Permission permission){

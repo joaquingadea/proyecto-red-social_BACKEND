9f654ae922c71104dc181538367d875ed5a5d31b
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-11 23:34:49 -0300

    Creacion de metodos registerUser, deleteById, existsById
    Interfaz IUserService.java

--- demo/src/main/java/com/red_social/demo/service/IUserService.java
+++ demo/src/main/java/com/red_social/demo/service/IUserService.java
@@ -1,7 +1,11 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.dto.AuthRegisterRequestDTO;
+import com.red_social.demo.dto.AuthRegisterResponseDTO;
 import com.red_social.demo.model.UserSec;
+import jakarta.validation.Valid;
 import org.jspecify.annotations.Nullable;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -11,4 +15,8 @@ public interface IUserService {
     List<UserSec> findAll();
 
     UserSec create(UserSec userSec);
+
+    AuthRegisterResponseDTO registerUser(AuthRegisterRequestDTO request);
+    void deleteById(Long id);
+    boolean existsById(Long id);
 }

a98ed28cd3f1d7aaa4b7553051dac3ade4bb941b
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-11 21:11:33 -0300

    se implementaron los metodos authenticate para verificar si el user existe, y en caso que el user exista, la contraseña coincida. Y el metodo loginUser donde se implementa el metodo authenticate, en este metodo se autentica el usuario la contraseña y se settea como authentication en el SecurityContextHolder, se crea el JWT y se devuelve en el header de la response

--- demo/src/main/java/com/red_social/demo/service/UserDetailsServiceImp.java
+++ demo/src/main/java/com/red_social/demo/service/UserDetailsServiceImp.java
@@ -1,13 +1,22 @@
 package com.red_social.demo.service;
 
+import com.red_social.demo.dto.AuthLoginRequestDTO;
+import com.red_social.demo.dto.AuthLoginResponseDTO;
 import com.red_social.demo.model.UserSec;
 import com.red_social.demo.repository.IUserSecRepository;
+import com.red_social.demo.utils.JwtUtils;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,6 +27,10 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private IUserSecRepository userRepo;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -50,5 +63,26 @@ public class UserDetailsServiceImp implements UserDetailsService {
                 authorities
         );
 
+    }
+
+    public AuthLoginResponseDTO loginUser(@Valid AuthLoginRequestDTO request) {
+        String username = request.username();
+        String password = request.password();
+        Authentication authentication = this.authenticate(username,password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String accessToken = jwtUtils.createToken(authentication);
+        AuthLoginResponseDTO response = new AuthLoginResponseDTO(username,"Login successfull", accessToken, true);
+        return response;
+    }
+
+    public Authentication authenticate(String username, String password) {
+        UserDetails user = this.loadUserByUsername(username);
+        if (user == null){
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        if (!passwordEncoder.matches(password,user.getPassword())){
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        return new UsernamePasswordAuthenticationToken(username,user.getPassword(),user.getAuthorities());
     }
 }

8117f1257bab40e948af32334a62439229a23a04
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-11 23:28:31 -0300

    se creo el metodo findByName que devuelve un Optional<Role> en la interfaz IRoleRepository.java

--- demo/src/main/java/com/red_social/demo/repository/IRoleRepository.java
+++ demo/src/main/java/com/red_social/demo/repository/IRoleRepository.java
@@ -4,7 +4,10 @@ import com.red_social.demo.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IRoleRepository extends JpaRepository<Role,Long> {
     boolean existsByName(String name);
+    Optional<Role> findByName(String role);
 }

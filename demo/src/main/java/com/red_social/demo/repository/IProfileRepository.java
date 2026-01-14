ee5ce0d0182d5b68f66d6d32d50a98a98018a744
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-13 23:05:37 -0300

    se agrego metodo para buscar un perfil a traves de un usuario por su nombre de usuario
    IProfileRepository.java

--- demo/src/main/java/com/red_social/demo/repository/IProfileRepository.java
+++ demo/src/main/java/com/red_social/demo/repository/IProfileRepository.java
@@ -3,7 +3,9 @@ package com.red_social.demo.repository;
 import com.red_social.demo.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface IProfileRepository extends JpaRepository<Profile,Long> {
+    Optional<Profile> findByUserUsername(String userCreator);
 }

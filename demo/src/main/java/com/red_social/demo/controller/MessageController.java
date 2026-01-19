ba96c2883e118523ebf06cbf06cebbd607b78076
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-19 14:40:07 -0300

    se modifico el status code de algunas respuestas, se creo el endpoint getAllMessages para obtener todos los mensajes creados hasta el momento y se modifico el endpoint editById para su correcto funcionamiento
    MessageController.java

--- demo/src/main/java/com/red_social/demo/controller/MessageController.java
+++ demo/src/main/java/com/red_social/demo/controller/MessageController.java
@@ -3,6 +3,7 @@ package com.red_social.demo.controller;
 import com.red_social.demo.dto.CreateMessageRequestDTO;
 import com.red_social.demo.dto.CreateMessageResponseDTO;
 import com.red_social.demo.dto.EditMessageRequestDTO;
+import com.red_social.demo.dto.GetEditMessageResponseDTO;
 import com.red_social.demo.service.IMessageService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@PreAuthorize("hasAuthority('ROLE_USER')")
+@PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
 @RequestMapping("/message")
 public class MessageController {
 
@@ -22,8 +23,8 @@ public class MessageController {
 
     @PreAuthorize("hasAuthority('MESSAGE_CREATE')")
     @PostMapping
-    public ResponseEntity<CreateMessageResponseDTO> createMessage(@Valid @RequestBody CreateMessageRequestDTO message, Authentication authentication){
-        return ResponseEntity.status(HttpStatus.CREATED)
+    public ResponseEntity<CreateMessageResponseDTO> createMessage(@RequestBody CreateMessageRequestDTO message, Authentication authentication){
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(messageService
                         .create(message,authentication.getName()));
     }
@@ -33,6 +34,12 @@ public class MessageController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(messageService.getOwnMessages(authentication.getName()));
     }
+    @PreAuthorize("hasAuthority('MESSAGE_READ')")
+    @GetMapping
+    public ResponseEntity getAllMessages(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(messageService.getAllMessages());
+    }
 
     @PreAuthorize("hasAuthority('MESSAGE_DELETE') and @messageSecurity.isOwner(#id,authentication)")
     @DeleteMapping("/{id}")
@@ -40,11 +47,18 @@ public class MessageController {
         messageService.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Successfully deleted message");
     }
-    @PreAuthorize("hasAuthority('MESSAGE_EDIT') and @messageSecurity.isOwner(#id,authentication)")
+    @PreAuthorize("hasAuthority('MESSAGE_UPDATE') and @messageSecurity.isOwner(#id,authentication)")
     @PatchMapping("/edit/{id}")
     public ResponseEntity editById(@PathVariable Long id,
-                                   @RequestBody EditMessageRequestDTO requestEdit){
-        messageService.edit(id, requestEdit);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Successfully edited message");
+                                   @RequestBody EditMessageRequestDTO requestEdit,
+                                   Authentication authentication){
+        messageService.edit(id, requestEdit, authentication);
+        return ResponseEntity.status(HttpStatus.OK).body("Successfully edited message");
+    }
+    @PreAuthorize("hasAuthority('MESSAGE_UPDATE') and @messageSecurity.isOwner(#id,authentication)")
+    @GetMapping("/get-edit-message/{id}")
+    public ResponseEntity editById(@PathVariable Long id,Authentication authentication){
+        GetEditMessageResponseDTO messageResponseDTO = messageService.findById(id,authentication);
+        return ResponseEntity.status(HttpStatus.OK).body(messageResponseDTO);
     }
 }
